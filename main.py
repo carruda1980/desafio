bc62e66e6344e1016f540e9491908a3922396713
caugustoarruda <caugustogarruda@gmail.com>
2020-12-16 07:20:33 -0300

    Getters e Setters

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+# Em Python não existe atributos privados propriamente ditos, o que existe é uma convenção
+# Tambem não se utiliza metodos getter e setters
 class Jogo:
     __quantidade_dezenas = [6,7,8,9]
     __resultado = 0
     __total_jogos = 0
     __jogos = 0
+
+    def get__resultado(self):
+        return self.__resultado
+
+    def set__resultado(self, resultado):
+        self.__resultado = resultado
+
+    def get__total_jogos(self):
+        return self.__total_jogos
+
+    def set__total_jogos(self, total_jogos):
+        self.__total_jogos = total_jogos
+
